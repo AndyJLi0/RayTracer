0c1c3f3f270a4b3295a7e98532b80ea7a898ae80
Paingle <andyjiaanli@gmail.com>
2023-09-27 20:37:34 -0700

    vector and pixel class

--- main.cpp
+++ main.cpp
@@ -6,18 +6,18 @@ int main()
 
     int image_width = 256;
     int image_height = 256;
-
+    
     std::cout << "P3\n"
               << image_width << ' ' << image_height << "\n255\n";
 
-    for (int y = 0; y < image_height; y++)
+    for (int j = 0; j < image_height; ++j)
     {
-        std::clog << "\rScanlines Remaining: " << (image_height - y) << ' ' << std::flush;
-        for (int x = 0; x < image_width; x++)
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i)
         {
-            auto r = double(y) / (image_width - 1);
-            auto g = double(x) / (image_height - 1);
-            auto b = double(x) / (image_width - 1);
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0;
 
             int ir = static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
@@ -26,5 +26,6 @@ int main()
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
     std::clog << "\rDone.                 \n";
 }
