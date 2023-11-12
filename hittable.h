15f4f21cdbfa8089a7118ae10962ef8bdc74831a
Paingle <andyjiaanli@gmail.com>
2023-11-12 01:38:59 -0800

    refactor main with camera class

--- hittable.h
+++ hittable.h
@@ -23,7 +23,6 @@ class hittable {
 public:
   virtual ~hittable() = default;
 
-
   virtual bool hit(const ray &r, interval ray_t, hit_record &rec) const = 0;
 };
 
--- main.cpp
+++ main.cpp
@@ -1,74 +1,14 @@
 #include "utility.h"
-
-#include "color.h"
-#include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
-
-#include <iostream>
-
-color ray_color(const ray &r, const hittable &world) {
-  hit_record rec;
-  if (world.hit(r, interval(0, std::numeric_limits<double>::infinity()), rec)) {
-    return 0.5 * (rec.normal + color(1, 1, 1));
-  }
-  vec3 unit_direction = unit_vector(r.direction());
-  auto a = 0.5 * (unit_direction.y() + 1.0);
-  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
-
+#include "camera.h"
 int main() {
-
-  // IMAGE
-  double aspect_ratio = 16.0 / 9.0;
-
-  // MAKE SURE IMAGE_HEIGHT >= 1
-  int image_width = 400;
-  int image_height = static_cast<int>(image_width / aspect_ratio);
-  image_height = (image_height < 1) ? 1 : image_height;
-
-  // WORLD
   hittable_list world;
   world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-  // CAMERA
-  double focal_length = 1.0;
-  double viewport_height = 2.0;
-  auto viewport_width = aspect_ratio * viewport_height;
-  point3 camera_center = point3(0, 0, 0);
-
-  // VIEWPORT VECTORS
-  vec3 viewport_u = vec3(viewport_width, 0, 0);
-  vec3 viewport_v = vec3(0, -viewport_height, 0);
-
-  // VECTOR FOR PIXEL
-  auto pixel_delta_u = viewport_u / image_width;
-  auto pixel_delta_v = viewport_v / image_height;
-
-  // TOP LEFT PIXEL
-  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
-                             viewport_u / 2 - viewport_v / 2;
-  auto pixel1_0_0_loc =
-      viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-  // RENDER
-
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-  for (int j = 0; j < image_height; ++j) {
-    std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
-              << std::flush;
-    for (int i = 0; i < image_width; ++i) {
-      auto pixel_center =
-          pixel1_0_0_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-      auto ray_direction = pixel_center - camera_center;
-      ray r(camera_center, ray_direction);
-
-      color pixel_color = ray_color(r, world);
-      write_color(std::cout, pixel_color);
-    }
-  }
-
-  std::clog << "\rDone.                 \n";
+  camera cam;
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
+  cam.render(world);
 }
